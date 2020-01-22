96d0fe074fa6b4b3f4e6f200e8056769eafbd835
Francis2023 <56661250+Francis2023@users.noreply.github.com>
2020-01-21 23:14:05 -0700

    not complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // Your code goes here
+const title = document.querySelector('logo-heading');
+const img = document.querySelectorAll('img');
+const nav = document.querySelector('nav');
+const navLink = document.querySelector('nav-link');
+const heads = document.querySelectorAll('h2');
+const button = document.querySelectorAll('btn');
+const bottomHeads = document.querySelectorAll('h4');
+
+title.addEventListener('select', (event) => {
+    event.target.color = "brown";
+})
+
+img[0].addEventListener('mouseover', () => {
+
+})
+
+navLink.addEventListener('drag', () => {
+    
+}) 
+
+nav.addEventListener('click', (event) => {
+    event.preventDefault();
+})
+
+bottomHeads.addEventListener('dblclick', (event) => {
+    event.target.style.color = 'brown'; 
+})
+
+heads.addEventListener('focus', () => {
+
+})
+
+img[1].addEventListener('resize', () => {
+    
+})
