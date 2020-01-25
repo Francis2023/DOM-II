b1bf5cfdd15fdeea446c431284cebc9fb3a82b67
Francis2023 <56661250+Francis2023@users.noreply.github.com>
2020-01-24 23:52:58 -0700

    second update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Your code goes here
-const title = document.querySelector('logo-heading');
+const title = document.querySelector('.logo-heading');
 const img = document.querySelectorAll('img');
 const nav = document.querySelector('nav');
 const navLink = document.querySelector('nav-link');
@@ -7,30 +7,43 @@ const heads = document.querySelectorAll('h2');
 const button = document.querySelectorAll('btn');
 const bottomHeads = document.querySelectorAll('h4');
 
-title.addEventListener('select', (event) => {
-    event.target.color = "brown";
+title.addEventListener('click', (event) => {
+    event.target.style.color = "blue";
 })
 
-img[0].addEventListener('mouseover', () => {
+img[0].addEventListener('mouseover', (event) => {
+    event.target.style.opacity = "0.5";
 
+   
 })
+img[0].addEventListener('mouseout', (event) => {
+    event.target.style.opacity = "1";
+})
+nav.addEventListener('dblclick', (event) => {
+    event.target.style.transform = 'scale(1.3)';
 
-navLink.addEventListener('drag', () => {
-    
+    setTimeout(() => {
+        event.target.style.transform = "";
+        }, 200);
 }) 
 
 nav.addEventListener('click', (event) => {
     event.preventDefault();
 })
 
-bottomHeads.addEventListener('dblclick', (event) => {
-    event.target.style.color = 'brown'; 
+bottomHeads.addEventListener('load', (event) => {
+  log.textContent = ''; 
+})
+button.addEventListener('keydown', (event) =>{
+    event.target.log.textContent += '${e.code}'
 })
 
-heads.addEventListener('focus', () => {
+//heads.addEventListener('focus', () => {
 
-})
+//})
 
-img[1].addEventListener('resize', () => {
-    
+img[1].addEventListener('blur', () => {
+    document.body.classList.add('paused');
+    log.textContent = 'No focus!';
+
 })
